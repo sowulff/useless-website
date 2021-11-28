ba1ce74b5b9b5a7102aefe6b968cc48124d9ee28
sowulff <sowu99@gmail.com>
2021-11-28 13:24:08 +0100

    parallax header in the making

---
+++ app.js
@@ -0,0 +1,14 @@
+let didScroll = false;
+function scrollBanner() {
+  var scrollPos;
+  var headerText = document.querySelector('.header-parallax h1');
+  scrollPos = window.scrollY;
+
+  if (scrollPos <= 600) {
+    headerText.style.transform = "translateY(" + (-scrollPos / 3) + "px" + ")";
+    headerText.style.opacity = 1 - (scrollPos / 600);
+  }
+}
+window.addEventListener('scroll', scrollBanner);
+
+console.log('hej')
